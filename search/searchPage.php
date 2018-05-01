b37e4370cb6a2f6a8a74124a1d810ab07e262f69
Ran Chen <curkychen@gmail.com>
2018-04-30 21:19:47 -0400

    Modifying search

--- search/searchPage.php
+++ search/searchPage.php
@@ -93,43 +93,7 @@
                 followPeople($dbc,$postUser, $user2Id);
             }
         }
-//        $sql = "select * from posts where ".$searchQuery." order by votes desc";
-//        $result = @mysqli_query($dbc, $sql);
-//        if (mysqli_num_rows($result) >= 1) {
-            while($row = mysqli_fetch_assoc($result)) {
-                if(isset($_SESSION['login_user'])){
-                    $postUser = $_SESSION['login_user'];
-                    echo "<li class=\"list-group-item\">
-                       <h3>".$row["title"]."</h3>
-                       <p>".$row["postDate"]."</p>
-                       <p>".$row["Content"]."</p>";
-                    $vote = $row["votes"];
-                    echo "<button id=\"btnfun\" name=\"btnfun\" onClick='location.href=\"?button".$row["postId"]."=1\"'>Vote</button>";
-                    if($_GET['button'.$row["postId"]]) {
-                        $vote = $vote + 1;
-                        $sql2 = "update posts set votes = .".$vote . " where postId = " . $row["postId"];
-                        $result2 = @mysqli_query($dbc, $sql2);
-                    }
-                    $sql3 = "select * from follow where userId1 = ".$postUser." and userId2 = ".$row["userId"];
-                    $result3 =  @mysqli_query($dbc, $sql3);
-                    if(mysqli_num_rows($result3) == 0) {
-                        echo "<button id=\"btnfun2\" name=\"btnfun2\" onClick='location.href=\"?button_follow".$row["userId"]."=1\"'>Follow the Author</button>";
-                        if($_GET['button_follow'.$row["userId"]]) {
-                            $sql4 = "insert into follow (userId1, userId2) values (".$postUser.",".$row["userId"].")";
-                            $result2 = @mysqli_query($dbc, $sql);
-                        }
-                    }
-                    echo "<a href=\"otherUserProfile.php?userId=".$row["postId"]."\">View the author profile</a>";
-                    echo "<p><a href=\"addTag.php?\postId=".$row["postId"].">Add to favorite</p>";
-                    echo "</li>";
-                } else {
-                    echo "<li class=\"list-group-item\">
-                       <h3>" . $row["title"] . "</h3>
-                       <p>" . $row["postDate"] . "</p>
-                       <p>" . $row["Content"] . "</p>";
-                    echo "</li>";
-                }
-//            }
+
         mysqli_close($dbc);
     }
     ?>
